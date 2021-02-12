a3bba7906f1c44d3c069a663b5276f73289a32de
ChazzIV <chazz.iv@gmail.com>
2021-02-12 13:07:37 -0600

    integración con angular material

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -7,12 +7,24 @@ import { ActivateComponent } from './activate/activate.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { LoginComponent } from './login/login.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [RegisterComponent, ActivateComponent, PasswordResetComponent, LoginComponent],
+  exports:[RegisterComponent, ActivateComponent, PasswordResetComponent, LoginComponent],
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule
   ]
 })
 export class AccountModule { }
