2c08df5295d0a61b49a3c6451ab44fdbe8523bd4
Marlon Yepes Ceballos <myepes82@misena.edu.co>
2021-02-11 22:55:20 -0500

    layouts directory has created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { HomeComponent } from './layouts/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
